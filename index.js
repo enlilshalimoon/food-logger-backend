2187d58e8f3a8c9b5c1c9a8c0ff152de274acebb
eshalimoon <71665665+eshalimoon@users.noreply.github.com>
2025-01-28 22:58:56 -0800

    added new api call to entering food via text

--- index.js
+++ index.js
@@ -4,30 +4,29 @@ import { fileURLToPath } from "url";
 import express from "express";
 import cors from "cors";
 import visionRoutes from "./routes/visionRoutes.js";
+import macroRoutes from "./routes/macroRoutes.js";
 
-// Recreate __dirname for ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Explicitly load .env
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Use vision routes for all `/api` endpoints
+// Mount vision routes
 app.use("/api", visionRoutes);
 
-// Root route for testing server
+// Mount macro routes
+app.use("/api", macroRoutes);
+
 app.get("/", (req, res) => {
     res.send("Food Logger Backend Running!");
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
